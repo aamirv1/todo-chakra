01cc03709179f13fe626b7572f71254c9efdcc1d
Aamir <aaimrmakofficial@gmail.com>
2022-07-15 11:41:33 +0530

    fix: error in build on index

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,311 @@
-import { Heading, VStack, IconButton, useColorMode } from "@chakra-ui/react";
-import TodoList from "./TodoList";
-import AddTodo from "./AddTodo";
-import { FaSun, FaMoon } from "react-icons/fa";
-import { useState, useEffect } from "react";
+import { RepeatClockIcon } from "@chakra-ui/icons";
+import { Box, Button, Divider, Heading, Image, Text } from "@chakra-ui/react";
 
-export default function Home() {
-  const initialTodos = [
-    { id: 1, body: "Get Todo Item 1" },
-    {
-      id: 2,
-      body: "Get Todo Item 2",
-    },
-  ];
-
-  const [todos, settodos] = useState(
-    // () => JSON.parse(localStorage.getItem("todos")) || []
-    initialTodos
-  );
-
-  // useEffect(() => {
-  //   localStorage.setItem("todos", JSON.stringify(todos));
-  // }, [todos]);
-
-  function deleteTodo(id) {
-    const newTodos = todos.filter((todo) => {
-      return todo.id !== id;
-    });
-    settodos(newTodos);
-  }
-  function addTodo(todo) {
-    settodos([...todos, todo]);
-  }
-
-  const { colorMode, toggleColorMode } = useColorMode();
-
+const test2 = () => {
   return (
-    <VStack p={4}>
-      <IconButton
-        size="lg"
-        alignSelf="flex-end"
-        icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
-        isRound="true"
-        onClick={toggleColorMode}
-      />
-      <Heading
-        mb="8"
-        fontWeight="extrabold"
-        size="2xl"
-        bgGradient="linear(to-r, pink.500, pink.300 , blue.500)"
-        bgClip="text"
-      >
-        Todo List
-      </Heading>
-      <TodoList todos={todos} deleteTodo={deleteTodo} />
-      <AddTodo addTodo={addTodo} />
-    </VStack>
+    <Box height={"full"} bg="#F0F2F5" pb="100px">
+      <Box display={"flex"} mb="60px" justifyContent="center" pt="200px">
+        <Box width="640px" height="612px" bg="#E4E2EF"></Box>
+        <Box width="640px" pl="60px" height="612px" bg="#FFFFFF">
+          <Text display={"inline-block"} size="16px" pt="50px" pb="24px">
+            In{" "}
+          </Text>{" "}
+          <Text color="#8169FF" display={"inline-block"}>
+            Art & Design
+          </Text>
+          <Heading size="xl" fontSize={"36px"} pb="37px" pr="60px">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint.
+          </Heading>
+          <Box mb="48px" display={"flex"}>
+            <Image
+              src="https://blog.internetfolks.in/content/images/2022/05/Icon.png"
+              width={"45px"}
+              height="45px"
+              borderRadius={"20px"}
+              mr="14px"
+            />
+            <Box mt="-1px">
+              <Text opacity={"0.5"} display="inline-block">
+                by{" "}
+              </Text>{" "}
+              <Text display="inline-block"> Jenny Wilson</Text>
+              <Text>July 6, 2022 . 0 Comments</Text>
+            </Box>
+          </Box>
+          <Text opacity={0.6} fontSize="15px" mr="79px">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </Text>
+        </Box>
+      </Box>
+      {/* First Box End */}
+      <Box display={"flex"} justifyContent="space-evenly" mb="60px" mx="260px">
+        <Box display={"flex"} flexDirection="column" justifyContent="center">
+          <Box width="396px" height="266px" bg="#E4E2EF"></Box>
+          <Box width="396px" pl="35px" height="500px" bg="#FFFFFF">
+            <Text display={"inline-block"} size="16px" pt="35px" pb="15px">
+              In{" "}
+            </Text>{" "}
+            <Text color="#8169FF" display={"inline-block"}>
+              Art & Design
+            </Text>
+            <Heading size="lg" fontSize={"24px"} pr="35px" pb="37px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint.
+            </Heading>
+            <Box mb="37px" display={"flex"}>
+              <Image
+                src="https://blog.internetfolks.in/content/images/2022/05/Icon.png"
+                width={"45px"}
+                height="45px"
+                borderRadius={"20px"}
+                mr="14px"
+              />
+              <Box mt="-1px">
+                <Text opacity={"0.5"} display="inline-block">
+                  by{" "}
+                </Text>{" "}
+                <Text display="inline-block"> Jenny Wilson</Text>
+                <Text>July 6, 2022 . 0 Comments</Text>
+              </Box>
+            </Box>
+            <Text opacity={0.6} fontSize="md" mb="30px" mr="79px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </Text>
+            <Divider mb="20px" />
+            <Box display={"flex"}>
+              <Button display={"inline-block"} mr="90px" size="md">
+                Continue Reading
+              </Button>
+              <Divider
+                display={"inline-block"}
+                height="42px"
+                orientation="vertical"
+              />
+              <Box ml="22px">
+                <RepeatClockIcon />
+                <Text>2 min</Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        {/* 2nd line box 2 */}
+        <Box display={"flex"} flexDirection="column" justifyContent="center">
+          <Box width="396px" height="266px" bg="#E4E2EF"></Box>
+
+          <Box width="396px" pl="35px" height="500px" bg="#FFFFFF">
+            <Text display={"inline-block"} size="16px" pt="35px" pb="15px">
+              In{" "}
+            </Text>{" "}
+            <Text color="#8169FF" display={"inline-block"}>
+              Art & Design
+            </Text>
+            <Heading size="lg" fontSize={"24px"} pr="35px" pb="37px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint.
+            </Heading>
+            <Box mb="37px" display={"flex"}>
+              <Image
+                src="https://blog.internetfolks.in/content/images/2022/05/Icon.png"
+                width={"45px"}
+                height="45px"
+                borderRadius={"20px"}
+                mr="14px"
+              />
+              <Box mt="-1px">
+                <Text opacity={"0.5"} display="inline-block">
+                  by{" "}
+                </Text>{" "}
+                <Text display="inline-block"> Jenny Wilson</Text>
+                <Text>July 6, 2022 . 0 Comments</Text>
+              </Box>
+            </Box>
+            <Text opacity={0.6} fontSize="md" mb="30px" mr="79px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </Text>
+            <Divider mb="20px" />
+            <Box display={"flex"}>
+              <Button display={"inline-block"} mr="90px" size="md">
+                Continue Reading
+              </Button>
+              <Divider
+                display={"inline-block"}
+                height="42px"
+                orientation="vertical"
+              />
+              <Box ml="22px">
+                <RepeatClockIcon />
+                <Text>2 min</Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        {/* 2nd line box 3 */}
+        <Box display={"flex"} flexDirection="column" justifyContent="center">
+          <Box width="396px" height="266px" bg="#E4E2EF"></Box>
+          <Box width="396px" pl="35px" height="500px" bg="#FFFFFF">
+            <Text display={"inline-block"} size="16px" pt="35px" pb="15px">
+              In{" "}
+            </Text>{" "}
+            <Text color="#8169FF" display={"inline-block"}>
+              Art & Design
+            </Text>
+            <Heading size="lg" fontSize={"24px"} pr="35px" pb="37px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint.
+            </Heading>
+            <Box mb="37px" display={"flex"}>
+              <Image
+                src="https://blog.internetfolks.in/content/images/2022/05/Icon.png"
+                width={"45px"}
+                height="45px"
+                borderRadius={"20px"}
+                mr="14px"
+              />
+              <Box mt="-1px">
+                <Text opacity={"0.5"} display="inline-block">
+                  by{" "}
+                </Text>{" "}
+                <Text display="inline-block"> Jenny Wilson</Text>
+                <Text>July 6, 2022 . 0 Comments</Text>
+              </Box>
+            </Box>
+            <Text opacity={0.6} fontSize="md" mb="30px" mr="79px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </Text>
+            <Divider mb="20px" />
+            <Box display={"flex"}>
+              <Button display={"inline-block"} mr="90px" size="md">
+                Continue Reading
+              </Button>
+              <Divider
+                display={"inline-block"}
+                height="42px"
+                orientation="vertical"
+              />
+              <Box ml="22px">
+                <RepeatClockIcon />
+                <Text>2 min</Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      {/* 2nd row end */}
+      <Box display={"flex"} justifyContent="space-evenly" mx="260px">
+        <Box display={"flex"} flexDirection="column" justifyContent="center">
+          <Box width="617px" height="410px" bg="#E4E2EF"></Box>
+          <Box width="617px" pl="45px" height="510px" bg="#FFFFFF">
+            <Text display={"inline-block"} size="16px" pt="45px" pb="20px">
+              In{" "}
+            </Text>{" "}
+            <Text color="#8169FF" display={"inline-block"}>
+              Art & Design
+            </Text>
+            <Heading size="lg" fontSize={"30px"} pr="52px" pb="32px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint.
+            </Heading>
+            <Box mb="42px" display={"flex"}>
+              <Image
+                src="https://blog.internetfolks.in/content/images/2022/05/Icon.png"
+                width={"45px"}
+                height="45px"
+                borderRadius={"20px"}
+                mr="14px"
+              />
+              <Box mt="-1px">
+                <Text opacity={"0.5"} display="inline-block">
+                  by{" "}
+                </Text>{" "}
+                <Text display="inline-block"> Jenny Wilson</Text>
+                <Text>July 6, 2022 . 0 Comments</Text>
+              </Box>
+            </Box>
+            <Text opacity={0.6} fontSize="md" mb="40px" mr="79px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </Text>
+            <Divider mb="24px" />
+            <Box display={"flex"}>
+              <Button display={"inline-block"} mr="290px" size="md">
+                Continue Reading
+              </Button>
+              <Divider
+                display={"inline-block"}
+                height="42px"
+                orientation="vertical"
+              />
+              <Box ml="22px">
+                <RepeatClockIcon />
+                <Text>2 min</Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box display={"flex"} flexDirection="column" justifyContent="center">
+          <Box width="617px" height="410px" bg="#E4E2EF"></Box>
+          <Box width="617px" pl="45px" height="510px" bg="#FFFFFF">
+            <Text display={"inline-block"} size="16px" pt="45px" pb="20px">
+              In{" "}
+            </Text>{" "}
+            <Text color="#8169FF" display={"inline-block"}>
+              Art & Design
+            </Text>
+            <Heading size="lg" fontSize={"30px"} pr="52px" pb="32px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint.
+            </Heading>
+            <Box mb="42px" display={"flex"}>
+              <Image
+                src="https://blog.internetfolks.in/content/images/2022/05/Icon.png"
+                width={"45px"}
+                height="45px"
+                borderRadius={"20px"}
+                mr="14px"
+              />
+              <Box mt="-1px">
+                <Text opacity={"0.5"} display="inline-block">
+                  by{" "}
+                </Text>{" "}
+                <Text display="inline-block"> Jenny Wilson</Text>
+                <Text>July 6, 2022 . 0 Comments</Text>
+              </Box>
+            </Box>
+            <Text opacity={0.6} fontSize="md" mb="40px" mr="79px">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </Text>
+            <Divider mb="24px" />
+            <Box display={"flex"}>
+              <Button display={"inline-block"} mr="290px" size="md">
+                Continue Reading
+              </Button>
+              <Divider
+                display={"inline-block"}
+                height="42px"
+                orientation="vertical"
+              />
+              <Box ml="22px">
+                <RepeatClockIcon />
+                <Text>2 min</Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
-}
+};
+
+export default test2;
